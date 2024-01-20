b1c4adcb79d4ccd11bb0f960209a7f7d61ecffb6
soya <3100034521@qq.com>
2024-01-20 13:44:59 +0800

    fix search

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -16,11 +16,26 @@ export default function Home() {
     queryFn: () => fetch('/api').then((res) => res.json()),
     staleTime: 1,
   })
+  console.log('🚀 ~ Home ~ data:', isPending, data)
 
   const isMobile = useIsMobile()
   return (
     <>
-      {isPending || (Array.isArray(data) && <div>Loading...</div>)}
+      {isPending && (
+        <div className="h-[100vh] w-[100vw] d-center">
+          <div className="absolute  !left-[50%] !top-[50%]   h-[200px] w-[200px] -translate-x-[50%] -translate-y-[50%]">
+            <img
+              src="/img/Boy_normal@2x.93f1447.png"
+              className="default-img inset-0"
+            />
+            <img
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAZAAAAGQBAMAAABykSv/AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAPUExURUxpcQBZ7gBZ7gBZ7gBZ7pmvuWsAAAAFdFJOUwAdqEh5pgqJvgAAAZVJREFUeNrt2oGNnDAUBFDQXgE4NJAQCjjOFJCA+68p9iY1RGL0Xgej/8crL54mAAAAAAAAAAAAAAAAAAAAAAAAAID/a57njCBLykCmpQvIsZSMgZSylISJzKWUiLKXIWG73kECJvI61pHk+UHWOoIEVL3Wox4BQdbak6zP36z5rMPzB/K6R5KAqp+t9SSfz9+s1u77Dtisj9adR8Rm9ZkkbNa+9yDPH8jHtl2t/X5+kK/t6kkCNmvr9j1is7qIzRpCNmuL2awfzw/yLWSzpusdJOCynrJZfyvyK+XwnUIq8jOgIu8g3wMq0kaQ598N+51qj/hZn1q7roTDd24jScDh++pB9j2gIuv7b4eAitTzjLitz7XeLeC2PoL0kQRU5FX7SGpARcZnkRpQkakcPchnQpCMzzvvRwJrQkV6kOM4MoKUsiRUpDsSgiwpE1mWiFcb/5IkdH1YppCRLBkxUh7IxphNBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIBUfwBvDC+COA1OnwAAAABJRU5ErkJggg=="
+              className="load-loop"
+              alt=""
+            />
+          </div>
+        </div>
+      )}
       {!isPending && data && (
         <Fragment>
           <SwiperTop banner_list={data?.banner || []} />
